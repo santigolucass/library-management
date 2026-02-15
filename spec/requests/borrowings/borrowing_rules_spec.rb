7876c312e7732b10e94542ae8f08f1c621200523
Lucas Santiago <lucas.stgom@gmail.com>
2026-02-15 09:28:16 -0300

    Adds tests to borrowing rules spec

--- spec/requests/borrowings/borrowing_rules_spec.rb
+++ spec/requests/borrowings/borrowing_rules_spec.rb
@@ -42,12 +42,18 @@ RSpec.describe "Borrowing rules", type: :request do
     librarian_headers = auth_headers_for(email: librarian.email, password: "password123")
 
     post "/api/v1/borrowings/borrow", params: { book_id: book.id }, headers: member_headers, as: :json
+    borrowing_payload = json_response.fetch("data")
     borrowing_id = json_response.fetch("data").fetch("id")
+    expect(borrowing_payload.dig("user", "email")).to eq(member.email)
+    expect(borrowing_payload.dig("book", "title")).to eq(book.title)
     expect(book.reload.available_copies).to eq(0)
 
     post "/api/v1/borrowings/#{borrowing_id}/return", headers: librarian_headers, as: :json
 
     expect(response).to have_http_status(:ok)
+    returned_payload = json_response.fetch("data")
+    expect(returned_payload.dig("user", "email")).to eq(member.email)
+    expect(returned_payload.dig("book", "title")).to eq(book.title)
     expect(book.reload.available_copies).to eq(1)
   end
 end
